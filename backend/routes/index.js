f29c20a9390ffe457c606afceafbc6c171d0ef89
Larry <you@example.comlarrysyliao@gmail.com>
2022-08-06 16:11:46 -0400

    phase 1 done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -17,4 +17,11 @@ router.get("/api/csrf/restore", (req, res) => {
     });
   });
 
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
 module.exports = router;
