4e1d56563d691ab67f46ea7a6002a06cc59a46df
Larry <you@example.comlarrysyliao@gmail.com>
2022-08-29 16:51:57 -0400

    credentials

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -28,7 +28,7 @@ router.post('/', validateLogin,  async (req, res, next) => {
       const err = new Error('Login failed');
       err.status = 401;
       err.title = 'Login failed';
-      err.errors = ['The provided emails were invalid.'];
+      err.errors = ['The provided credentials were invalid.'];
       return next(err);
     }
     
